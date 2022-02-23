2942fb779db14b24401977681c6b75d2ce51672f
Andriatsimaniry <niryomega@free.fr>
2022-02-23 22:08:37 +0100

    mise en page Bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,35 +4,53 @@ import { StaticImage } from "gatsby-plugin-image";
 import { Link } from "gatsby";
 import { getUser, isLoggedIn } from "../services/auth";
 import DogVideo from "../assets/video.mp4";
+import { Row, Col, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Page d'Acceuil G.A.M">
-      <h1>Bienvenue sur le Site du G.A.M</h1>
-      <StaticImage alt="La Rotonde" src="../images/Rotonde1.png" />
-      <h1>Bonjour {isLoggedIn() ? getUser().name : "GAM"}!</h1>
-      <p>
-        {isLoggedIn() ? (
-          <>
-            Vous êtes connecté, alors vérifiez votre{" "}
-            <Link to="/app/profile">profil</Link>
-          </>
-        ) : (
-          <>
-            Tu devrais <Link to="/app/login">Connecter</Link>Pour voir le
-            contenu restreint
-          </>
-        )}
-      </p>
-      <section>
-        <p>Juste une petite video pour commencer !</p>
-
-        <video controls>
-          <source src={DogVideo} type="video/mp4" />
-        </video>
-      </section>
-    </Layout>
+    <Container fluid>
+      <Layout pageTitle="Page d'Acceuil G.A.M">
+        <Row className="justify-content-md-center">
+          <Col xs lg="2">
+            <h1>Bienvenue</h1>
+          </Col>
+          <Col md="auto">
+            <video controls>
+              <source src={DogVideo} type="video/mp4" />
+            </video>
+          </Col>
+          <Col xs lg="2">
+            La Rotonde
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {" "}
+            <p>
+              {isLoggedIn() ? (
+                <>
+                  Vous êtes connecté, alors vérifiez votre{" "}
+                  <Link to="/app/profile">profil</Link>
+                </>
+              ) : (
+                <>
+                  Tu devrais<Link to="/app/login">Connecter</Link>Pour voir le
+                  contenu restreint. Nous sommes heureux de vous partager les
+                  moments
+                </>
+              )}
+            </p>{" "}
+          </Col>
+          <Col md="auto">
+            <StaticImage alt="La Rotonde" src="../images/Rotonde1.png" />
+          </Col>
+          <Col xs lg="2">
+            <h5>Bonjour{isLoggedIn() ? getUser().name : "GAM"}!</h5>
+          </Col>
+        </Row>
+      </Layout>
+    </Container>
   );
 };
 
