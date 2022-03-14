d1ae44a5235cf3cc302deaad00e75bfa07963550
Andriatsimaniry <niryomega@free.fr>
2022-03-14 11:00:29 +0100

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
           pngOptions: {},
           webpOptions: {},
           avifOptions: {},
+          
         },
       },
     },
@@ -58,6 +59,25 @@ module.exports = {
               ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
               containerClass: `embedVideo-container`, //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
               iframeId: false, //Optional: if true, iframe's id will be set to what is provided after 'video:' (YouTube IFrame player API requires iframe id)
+            },
+          },
+
+          {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+              name: "GatsbyJS",
+              short_name: "GatsbyJS",
+              start_url: "/",
+              background_color: "#6b37bf",
+              theme_color: "#6b37bf",
+              // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+              // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+              display: "standalone",
+              icon: "src/images/icon.png", // This path is relative to the root of the site.
+              // An optional attribute which provides support for CORS check.
+              // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+              // Any invalid keyword or empty string defaults to `anonymous`
+              crossOrigin: `use-credentials`,
             },
           },
         ],
--- src/containers/footer.js
+++ src/containers/footer.js
@@ -16,7 +16,7 @@ export function FooterContainer() {
             <Footer.Link href="/blog/calendrier/">Histoire</Footer.Link>
             <Footer.Link href="/blog/liste-des-membres/">Adhérent</Footer.Link>
             <Footer.Link href="/about">Projet</Footer.Link>
-            <Footer.Link href="https://www.diplomatie.gov.mg/index.php/liens-utiles">
+            <Footer.Link href="https://www.diplomatie.gov.mg/index.php/liens-utiles" rel="noopener noreferrer" target="_blank">
               Liens Utiles
             </Footer.Link>
             </Footer.Column>
@@ -24,16 +24,16 @@ export function FooterContainer() {
           
           <Footer.Column>
             <Footer.Title>Presses</Footer.Title>            
-            <Footer.Link href="https://www.midi-madagasikara.mg/">
+            <Footer.Link href="https://www.midi-madagasikara.mg/" rel="noopener noreferrer" target="_blank">
               Midi madagascar
             </Footer.Link>
-            <Footer.Link href="https://www.madagascar-tribune.com/">
+            <Footer.Link href="https://www.madagascar-tribune.com/" rel="noopener noreferrer" target="_blank">
               madagascar Tribune
             </Footer.Link>
-            <Footer.Link href="https://www.orange.mg/">
+            <Footer.Link href="https://www.orange.mg/" rel="noopener noreferrer" target="_blank">
               Orange madagascar
             </Footer.Link>
-            <Footer.Link href="https://www.airmadagascar.com/">
+            <Footer.Link href="https://www.airmadagascar.com/" rel="noopener noreferrer" target="_blank">
               Air madagascar
             </Footer.Link>
             {/* <span className={Vertical}></span> */}
@@ -53,16 +53,16 @@ export function FooterContainer() {
           
           <Footer.Column>
             <Footer.Title>Social</Footer.Title>
-            <Footer.Link href="https://www.Facebook.com/">
+            <Footer.Link href="https://www.Facebook.com/" rel="noopener noreferrer" target="_blank">
               <FaFacebook size="20" color="rgba(255, 255, 255, 0.5)" /> Facebook
             </Footer.Link>
-            <Footer.Link href="https://www.instagram.com/">
+            <Footer.Link href="https://www.instagram.com/" rel="noopener noreferrer" target="_blank">
               <FaInstagram size="20" color="rgba(255, 255, 255, 0.5)" /> Instagram
             </Footer.Link>
-            <Footer.Link href="https://www.youtube.com/watch?v=Igihdg_uW94">
+            <Footer.Link href="https://www.youtube.com/watch?v=Igihdg_uW94" rel="noopener noreferrer" target="_blank">
               <FaYoutube size="20" color="rgba(255, 255, 255, 0.5)" /> Youtube
             </Footer.Link>
-            <Footer.Link href="https://twitter.com/Randria73775597">
+            <Footer.Link href="https://twitter.com/Randria73775597" rel="noopener noreferrer" target="_blank">
               <FaTwitter size="20" color="rgba(255, 255, 255, 0.5)" /> Twitter
             </Footer.Link>
           </Footer.Column>
@@ -71,7 +71,7 @@ export function FooterContainer() {
       {/* <p className={Copyright}> */}
       <Footer.Copyright>
       Copyright © {new Date().getFullYear()}, <FaHeart color="rgba(255, 255, 255, 0.5)" />.
-        NiryOmega, All rights reserved. 
+        NiryOmega, Tous droits réservés. 
       </Footer.Copyright>
         
       
