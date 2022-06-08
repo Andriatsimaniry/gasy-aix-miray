65e209b921803674af34b13c7a1defd7ebcda668
Andriatsimaniry <niryomega@free.fr>
2022-06-08 15:35:14 +0200

    maj

--- src/pages/adherent.js
+++ src/pages/adherent.js
@@ -8,6 +8,7 @@ const AdherentPage = () => {
         <table className="table table-hover">
           <thead>
             <tr>
+            <th>Nombre</th>
               <th>Nom</th>
               <th>Prénom</th>
               <th>Adhérent</th>
@@ -17,12 +18,14 @@ const AdherentPage = () => {
           </thead>
           <tbody>
             <tr>
+            <td>1</td>
               <td>RAKOTOMANGA</td>
               <td>Patrick</td>
               <td>Président</td>
               <td>06.66.69.37.50</td>
               <td>rakotozafy.eric@yahoo.fr</td>
             </tr><tr>
+            <td>2</td>
               <td>RAMIARISOA</td>
               <td>Roberthine</td>
               <td>Membre</td>
@@ -31,6 +34,7 @@ const AdherentPage = () => {
             </tr>
          
             <tr>
+            <td>3</td>
               <td>RANDRIAMBOLOLONTSOA</td>
               <td>Andriatsimaniry</td>
               <td>Vice-Président</td>
@@ -38,6 +42,7 @@ const AdherentPage = () => {
               <td>niryomega@free.fr</td>
             </tr>
             <tr>
+            <td>4</td>
               <td>RANDRIAMBOLOLONTSOA</td>
               <td>Noro</td>
               <td>Membre</td>
@@ -45,6 +50,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>5</td>
               <td>RAMANGASON</td>
               <td>Nirina</td>
               <td>Secrétaire</td>
@@ -52,6 +58,7 @@ const AdherentPage = () => {
               <td>nramangason@yahoo.com</td>
             </tr>
             <tr>
+            <td>6</td>
               <td>PAILLERON</td>
               <td>Christophe</td>
               <td>Membre</td>
@@ -59,6 +66,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>7</td>
               <td>REMACLY</td>
               <td>Nathalie</td>
               <td>Secrétaire adjointe</td>
@@ -66,6 +74,7 @@ const AdherentPage = () => {
               <td>nathalie.remacly@yahoo.fr</td>
             </tr>
             <tr>
+            <td>8</td>
               <td>MAURICE</td>
               <td>Joro Rojo</td>
               <td>Trésorière</td>
@@ -73,6 +82,7 @@ const AdherentPage = () => {
               <td>rojo.jessica@ymail.com</td>
             </tr>
             <tr>
+            <td>9</td>
               <td>RAHAERISOA</td>
               <td>Odette</td>
               <td>Trésorière adjointe</td>
@@ -80,6 +90,7 @@ const AdherentPage = () => {
               <td>odette.rahari@hotmail.fr</td>
             </tr>
             <tr>
+            <td>10</td>
               <td>RAHAERISOA</td>
               <td>Joe</td>
               <td>Membre</td>
@@ -87,6 +98,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>11</td>
               <td>RAKOTOZANANY</td>
               <td>Alain ADolphe</td>
               <td>Membre</td>
@@ -94,6 +106,7 @@ const AdherentPage = () => {
               <td>adolpherakotozanany4@gmail.com</td>
             </tr>
             <tr>
+            <td>12</td>
               <td>RAKOTOZANANY</td>
               <td>Holy</td>
               <td>Membre</td>
@@ -101,6 +114,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>13</td>
               <td>ANDRY</td>
               <td> Luc </td>
               <td>Conseiller</td>
@@ -108,6 +122,7 @@ const AdherentPage = () => {
               <td>zo_andri@yahoo.com</td>
             </tr>
             <tr>
+            <td>14</td>
               <td>ANDRY</td>
               <td>Rinah</td>
               <td>Membre</td>
@@ -115,6 +130,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>15</td>
               <td>ANDRIANIRIANA</td>
               <td>Marc</td>
               <td>Conseiller</td>
@@ -122,6 +138,7 @@ const AdherentPage = () => {
               <td>tyx13090@yahoo.fr</td>
             </tr>
             <tr>
+            <td>16</td>
               <td>RABARISON</td>
               <td>Hervé</td>
               <td>Conseiller</td>
@@ -129,6 +146,7 @@ const AdherentPage = () => {
               <td>doug_rabarison@yahoo.fr</td>
             </tr>
             <tr>
+            <td>17</td>
               <td>TOTOLO</td>
               <td>Patrice</td>
               <td>Conseiller</td>
@@ -136,6 +154,7 @@ const AdherentPage = () => {
               <td>patricet158@gmail.com</td>
             </tr>
             <tr>
+            <td>18</td>
               <td>RADAVIDRA</td>
               <td>Tahinaharifomba</td>
               <td>Conseiller</td>
@@ -143,6 +162,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>19</td>
               <td>RAOERA</td>
               <td>Liva</td>
               <td>Membre</td>
@@ -150,6 +170,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>20</td>
               <td>RAOERA</td>
               <td>Lova</td>
               <td>Membre</td>
@@ -159,6 +180,7 @@ const AdherentPage = () => {
             
            
             <tr>
+            <td>21</td>
               <td>HOUEIX</td>
               <td>Arlette</td>
               <td>Membre</td>
@@ -166,6 +188,7 @@ const AdherentPage = () => {
               <td>Arlettehouex77@gmail.com</td>
             </tr>
             <tr>
+            <td>22</td>
               <td>ANDRIANASOLOARIJAONA</td>
               <td>Christophe</td>
               <td>Conseiller</td>
@@ -173,6 +196,7 @@ const AdherentPage = () => {
               <td>crristophe@hotmail.com</td>
             </tr>
             <tr>
+            <td>23</td>
               <td>ANDRIAMBOLOLONA</td>
               <td>Hery</td>
               <td>Membre</td>
@@ -180,6 +204,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>24</td>
               <td>ANDRIAMBOLOLONA</td>
               <td>Haingo</td>
               <td>Membre</td>
@@ -188,6 +213,7 @@ const AdherentPage = () => {
             </tr>
             
             <tr>
+            <td>25</td>
               <td></td>
               <td>Hermine</td>
               <td>Membre</td>
@@ -195,6 +221,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>26</td>
               <td></td>
               <td>Tiana</td>
               <td>Membre</td>
@@ -203,6 +230,7 @@ const AdherentPage = () => {
             </tr>
 
             <tr>
+            <td>27</td>
               <td></td>
               <td>Nomena</td>
               <td>Membre</td>
@@ -211,6 +239,7 @@ const AdherentPage = () => {
             </tr>
             
             <tr>
+            <td>28</td>
               <td>RATSIVOSON</td>
               <td>Fiononana</td>
               <td>Conseillère</td>
@@ -219,6 +248,7 @@ const AdherentPage = () => {
             </tr>
                       
             <tr>
+            <td>29</td>
               <td>RAKOTOARISON</td>
               <td>Ony</td>
               <td>Membre</td>
@@ -226,6 +256,7 @@ const AdherentPage = () => {
               <td>arisonmiah@gmail.com</td>
             </tr>
             <tr>
+            <td>30</td>
               <td>CAMPO</td>
               <td>Nirina (Niks)</td>
               <td>Conseillère</td>
@@ -233,6 +264,7 @@ const AdherentPage = () => {
               <td>niks109@yahoo.fr</td>
             </tr>
             <tr>
+            <td>31</td>
               <td>TOLOJANAHARY</td>
               <td>Aiky</td>
               <td>Membre</td>
@@ -240,6 +272,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>32</td>
               <td>UBACH</td>
               <td> Haingotiana Malala </td>
               <td>Conseillère</td>
@@ -247,6 +280,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>33</td>
               <td></td>
               <td> Amélie</td>
               <td>Conseillère</td>
@@ -254,6 +288,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>34</td>
               <td></td>
               <td> Sabine </td>
               <td>Membre</td>
@@ -261,6 +296,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>35</td>
               <td></td>
               <td> Nicole </td>
               <td>Membre</td>
@@ -268,6 +304,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>36</td>
               <td>RAKOTOZAFY</td>
               <td> Nandrasana </td>
               <td>Membre</td>
@@ -275,6 +312,7 @@ const AdherentPage = () => {
               <td></td>
             </tr>
             <tr>
+            <td>37</td>
               <td>RAKOTOZAFY</td>
               <td> Hanitra </td>
               <td>Membre</td>
